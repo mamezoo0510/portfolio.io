dde2ff592da7c3deec574cc6cf39cfd42fd5650a
mamezoo0510 <85331283+mamezoo0510@users.noreply.github.com>
2025-11-27 23:20:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// スクロールでフェードイン
+document.addEventListener('DOMContentLoaded', () => {
+  const faders = document.querySelectorAll('.fade');
+
+  const appearOptions = {
+    threshold: 0.1
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+});
